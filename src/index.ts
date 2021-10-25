56586a9c5ba3506b8e17a5ab90964eb0c42348ba
NadiaElso <92063948+NadiaElso@users.noreply.github.com>
2021-10-25 16:28:04 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+let tabla:number=document.getElementById("tabla");
+let hasta:number=document.getElementById("hasta");
+multiplicacion:number;
+multiplicacion=0;
+
+
+
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+  
+}
