a3a5c4c2b715bde2c1ebe3d2dc8b550e1893d290
NadiaElso <92063948+NadiaElso@users.noreply.github.com>
2021-10-25 16:36:27 -0300

    agreguéfor

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 let tabla:number=document.getElementById("tabla");
 let hasta:number=document.getElementById("hasta");
-multiplicacion:number;
-multiplicacion=0;
+let multiplicacion:number
+multiplicacion =0;
 
+let btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
 
-
-for (let index = 0; index < array.length; index++) {
-  const element = array[index];
+for (let hasta = 0; hasta=hasta.value; hasta++) {
+  multiplicacion =tabla.value*hasta;
+  hasta++;
   
 }
