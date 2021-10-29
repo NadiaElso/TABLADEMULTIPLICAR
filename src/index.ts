1fd11e1a985a492092be494d5aa30db49b19682a
NadiaElso <92063948+NadiaElso@users.noreply.github.com>
2021-10-29 18:05:08 -0300

    funciona

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
 let tabla:number=document.getElementById("tabla");
 let hasta:number=document.getElementById("hasta");
-let multiplicacion:number
-multiplicacion =0;
+let multiplicacion:number=0;
+
 
 let btn = document.getElementById("btn");
 btn.addEventListener("click", () => {
 
-  for (let contador:number = 1; contador:number <=hasta; contador:number++){
-    let multiplicacion =tabla.value*contador;
-    console.log(tabla,"x",contador,"=",resultado);
-   
-  }
-
+  for (let contador:number = 1; contador <=hasta.value; contador++){
+    let multiplicacion:number =tabla.value*contador;
+    console.log(tabla.value,"x",contador,"=",multiplicacion)
+   }
